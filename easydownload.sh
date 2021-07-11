8fb5aa1f678a4ea2dcdccbeabe8e5bd9dad075bc
pnchinmay <pnchinmay9@gmail.com>
2021-07-11 17:29:40 +0530

    Refactored code & added GUI for the end-users

--- easydownload.sh
+++ easydownload.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
-mkdir -p ~/Downloads/Swecha_videos
-echo "Enter URL:" | lolcat
-read URL
-echo "Enter the filename: " | lolcat
-read filename
-echo "Do you want to delete the residue files ? (y/n)" | lolcat
-read yesorno
-presentdir=$(pwd)
-#echo $presentdir
+mkdir -p ~/Downloads/bbb_downloader_videos
+URL=$(zenity --entry --text "Please enter the URL")
+filename=$(zenity --entry --text "Please enter filename")
+yesorno=$(zenity --entry --text "Do you want to delete the residue files ? (y/n)")
+(
+xmd=$(pwd)
+presentdir=$xmd
 $presentdir/download_bbb_data.py $URL
 file="./dirname.txt"
-dirname=$(cat "$file")
+dirname=$(/usr/bin/cat "$file")
 cd ./$dirname/Videos/
 $presentdir/integrate_soundtrack.sh
 $presentdir/webm_to_mp4.sh ./output.webm
 cp ./output.mp4 ./$filename.mp4
-mv ./$filename.mp4 ~/Downloads/Swecha_videos/
-echo "File moved to Downloads/Swecha_videos folder" | lolcat
+mv ./$filename.mp4 ~/Downloads/bbb_downloader_videos/
+killall zenity
+) | zenity --progress --pulsate --text "Downloading video"
+file="./dirname.txt"
+dirname=$(/usr/bin/cat "$file")
+zenity --info --text "File downloaded to ~/Downloads/bbb_downloader_videos folder"
 if [[ "$yesorno" = "y" ]]; then
-  echo $presentdir/$dirname
-  rm -rf $presentdir/$dirname
-  echo "Residual files removed" | lolcat
+  rm -rf $dirname
 fi
 if [[ "$yesorno" = "Y" ]]; then
-  echo $presentdir/$dirname
-  rm -rf $presentdir/$dirname
-  echo "Residual files removed" | lolcat
+  rm -rf $dirname
 fi
-#rm -rf ./$dirname/Videos/*.webm
-
-#/home/manas/scratch/20210710-1853/bbb-downloader/webm_to_mp4.sh ./$dirname/Videos/deskshare.webm
-#/home/manas/scratch/20210710-1853/bbb-downloader/webm_to_mp4.sh ./$dirname/Videos/webcams.webm
